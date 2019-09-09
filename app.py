787d14f2e3d993b0daa216fe91baa2d20c999f78
Beezai <beezai939@gmail.com>
2019-09-09 13:08:04 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import json
 from datetime import datetime
@@ -37,8 +38,8 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-
-                    send_message(sender_id, "hello : I am under development")
+                    msg = ["Hello", "Wassup", "Get lost", "How u been", "Cool"]
+                    send_message(sender_id, random.choice(msg))
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
