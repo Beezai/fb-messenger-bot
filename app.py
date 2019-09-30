201d1321a72cbbdfab0388518ad667172ebb0add
NiteshDhakal <niteshdhakal25@gmail.com>
2019-09-30 13:24:27 +1000

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ def verify():
 
 @app.route('/', methods=['POST'])
 def webhook():
-    def reply(msg):
-        return("HEllo", msg)
+#     def reply(msg):
+#         return("HEllo", msg)
 
     # endpoint for processing incoming messaging events
 
@@ -40,8 +40,8 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    text = reply(message_text)
-                    send_message(sender_id,text)
+#                     text = reply(message_text)
+                    send_message(sender_id,message_text)
                    
                     
                 if messaging_event.get("delivery"):  # delivery confirmation
